f9d9cb6ada0b141b04d87b38e2b0e237ed747c0e
Kyler <kylerlepp8@gmail.com>
2022-03-02 01:45:28 -0800

    ingredients part for and while loops

---
+++ ingredients.js
@@ -0,0 +1,17 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+/*for (i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+*/
+
+/*let i = 0
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+*/
+
+for (i = 0; i < ingredients.length; i++) {
+  console.log(ingredients.reverse(i));
+}
